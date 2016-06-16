4dfa9f5aecea5effed202404817b0a2decc8e0c6
pcego <pcego36@gmail.com>
2016-06-15 23:22:05 -0300

    add actions mark_as_paid

---
+++ eventex/subscriptions/tests/test_admin.py
@@ -0,0 +1,58 @@
+from unittest.mock import Mock
+
+from django.test import TestCase
+from eventex.subscriptions.admin import \
+    Subscription, SubscriptionModelAdmin, admin
+
+
+class SubscriptionModelAdminTest(TestCase):
+
+
+    def setUp(self):
+
+        Subscription.objects.create(name= 'Paulo',
+                                    cpf= '12345678901',
+                                    email= 'pcego36@gmail.com',
+                                    phone= '3832122980')
+
+        self.model_admin = SubscriptionModelAdmin(Subscription, admin.site)
+
+
+    def test_has_action(self):
+        """
+        action mark as paid should be installed
+        """
+        self.assertIn('mark_as_paid', self.model_admin.actions)
+
+
+    def test_mark_all(self):
+        """
+        It should mark all selected subscriptions as paid
+        """
+        self.call_action()
+        self.assertEqual(1, Subscription.objects.filter(paid=True).count())
+
+
+    def test_message(self):
+        """It should send a message to user"""
+
+        mock = self.call_action()
+        mock.assert_called_once_with(None, '1 Inscricao Marcada como Paga')
+
+
+    def call_action(self):
+
+        queryset = Subscription.objects.all()
+
+        mock = Mock()
+        old_message_user = SubscriptionModelAdmin.message_user
+        SubscriptionModelAdmin.message_user = mock
+
+        self.model_admin.mark_as_paid(None, queryset)
+        SubscriptionModelAdmin.message_user = old_message_user
+
+        return mock
+
+
+
+
